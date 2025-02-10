b5c19153d275816786c0a136c9288c9db279a640
Rowen23 <28rrogers@aos92.org>
2025-02-10 10:10:53 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
-let mySprite = sprites.create(assets.image`stick dude walking`, SpriteKind.Player)
-animation.runImageAnimation(
-mySprite,
-assets.animation`walking`,
-500,
-true
-)
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`walking`,
+    500,
+    true
+    )
+    mySprite.sayText("Throw ball", 2000, false)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`myAnim`,
+    200,
+    false
+    )
+    mySprite.sayText("walk", 1000, false)
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(assets.image`stick dude walking`, SpriteKind.Player)
